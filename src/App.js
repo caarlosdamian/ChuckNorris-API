19a90fcf02bd43e5f3c7a3101d7b92c206b0ecdd
caarlosdamian <dapecarlos@gmail.com>
2021-07-11 20:00:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Typography } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1" component="h2">
+        Hello App
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
